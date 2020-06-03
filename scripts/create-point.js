3ab2f8754521f52f0b9c786b5a594970ca31f173
Lays Hidani <lays.hidani@accenture.com>
2020-06-03 09:42:19 -0300

    refact(create-point): refact es6 :recycle:

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,19 +1,14 @@
-function populateUFs() {
+const populateUFs = (() => {
   const ufSelect = document.querySelector('select[name=uf]');
 
   fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
     .then(res => res.json())
     .then(states => {
-
-      for (const state of states) {
-        ufSelect.innerHTML += `<option value='${state.id}'>${state.nome}</option>`
-      }
+      states.forEach(state => ufSelect.innerHTML += `<option value='${state.id}'>${state.nome}</option>`);
     })
-}
+})();
 
-populateUFs();
-
-function getCities(event) {
+const getCities = (event) => {
   const citySelect = document.querySelector('[name=city]');
   const stateInput = document.querySelector('[name=state]');
 
@@ -24,22 +19,22 @@ function getCities(event) {
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
-
-  citySelect.innerHTML = '<option value>Selecione a Cidade</option>';
-  citySelect.disabled = true;
+  clearCitySelected(citySelect);
 
   fetch(url)
     .then(res => res.json())
     .then(cities => {
-
-      for (const city of cities) {
-        citySelect.innerHTML += `<option value='${city.id}'>${city.nome}</option>`;
-      }
+      cities.forEach(city => citySelect.innerHTML += `<option value='${city.id}'>${city.nome}</option>`);
 
       citySelect.disabled = false;
     })
-}
+};
+
+const clearCitySelected = (citySelect) => {
+  citySelect.innerHTML = '<option>Selecione a Cidade</option>';
+  citySelect.disabled = true;
+};
 
 document
   .querySelector('select[name=uf]')
-  .addEventListener('change', getCities)
+  .addEventListener('change', getCities);
